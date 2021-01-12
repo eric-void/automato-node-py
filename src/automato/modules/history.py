434731f497655e9ff61a7f8390804c2548aa935e
Eric <eric@void.it>
2021-01-12 10:40:31 +0100

    History module: thread locking

--- src/automato/modules/history.py
+++ src/automato/modules/history.py
@@ -4,6 +4,7 @@
 import logging
 import os
 import datetime
+import threading
 
 from automato.core import system
 from automato.core import utils
@@ -24,36 +25,39 @@ def load(entry):
   entry.history_path = os.path.realpath(os.path.join(entry.node_config['base_path'], entry.definition['config']['path']))
   entry.history_last_file_suffix = ''
   entry.history_event_buffer = {}
+  entry.history_event_buffer_lock = threading.Lock()
   system.on_all_events(lambda _entry, eventname, eventdata, caller, published_message: on_all_events(entry, _entry, eventname, eventdata, caller, published_message))
 
 def on_all_events(installer_entry, entry, eventname, eventdata, caller, published_message):
   if eventname not in installer_entry.config['ignore_events']:
-    if entry.id not in installer_entry.history_event_buffer:
-      installer_entry.history_event_buffer[entry.id] = {}
-    if eventname not in installer_entry.history_event_buffer[entry.id]:
-      installer_entry.history_event_buffer[entry.id][eventname] = {}
-    k = system.entry_event_keys_index(eventdata['keys'])
-    if k not in installer_entry.history_event_buffer[entry.id][eventname]:
-      installer_entry.history_event_buffer[entry.id][eventname][k] = [ ['#', eventdata] ]
-    else:
-      installer_entry.history_event_buffer[entry.id][eventname][k].append(['', eventdata])
+    with entry.history_event_buffer_lock:
+      if entry.id not in installer_entry.history_event_buffer:
+        installer_entry.history_event_buffer[entry.id] = {}
+      if eventname not in installer_entry.history_event_buffer[entry.id]:
+        installer_entry.history_event_buffer[entry.id][eventname] = {}
+      k = system.entry_event_keys_index(eventdata['keys'])
+      if k not in installer_entry.history_event_buffer[entry.id][eventname]:
+        installer_entry.history_event_buffer[entry.id][eventname][k] = [ ['#', eventdata] ]
+      else:
+        installer_entry.history_event_buffer[entry.id][eventname][k].append(['', eventdata])
 
 def run(entry):
   file_suffix = datetime.datetime.now().strftime('%Y-%m-%d')
-  for entry_id in entry.history_event_buffer:
-    lines = []
-    filepath = entry.history_path + '/' + entry_id + '-' + file_suffix + '.tsv'
+  with entry.history_event_buffer_lock:
+    for entry_id in entry.history_event_buffer:
+      lines = []
+      filepath = entry.history_path + '/' + entry_id + '-' + file_suffix + '.tsv'
 
-    for eventname in entry.history_event_buffer[entry_id]:
-      for k in entry.history_event_buffer[entry_id][eventname]:
-        t = '#'
-        for d in entry.history_event_buffer[entry_id][eventname][k]:
-          if file_suffix == entry.history_last_file_suffix:
-            t = d[0]
-          if t == '#' or d[1]['changed_params']:
-            lines.append(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f') + '\t' + eventname + '\t' + utils.json_export(d[1]['keys']) + '\t' + t + utils.json_export({x: d[1]['params'][x] for x in d[1]['params'] if x not in d[1]['keys']} if t == '#' else d[1]['changed_params']))
-          t = ''
-        entry.history_event_buffer[entry_id][eventname][k] = []
+      for eventname in entry.history_event_buffer[entry_id]:
+        for k in entry.history_event_buffer[entry_id][eventname]:
+          t = '#'
+          for d in entry.history_event_buffer[entry_id][eventname][k]:
+            if file_suffix == entry.history_last_file_suffix:
+              t = d[0]
+            if t == '#' or d[1]['changed_params']:
+              lines.append(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f') + '\t' + eventname + '\t' + utils.json_export(d[1]['keys']) + '\t' + t + utils.json_export({x: d[1]['params'][x] for x in d[1]['params'] if x not in d[1]['keys']} if t == '#' else d[1]['changed_params']))
+            t = ''
+          entry.history_event_buffer[entry_id][eventname][k] = []
     
     if len(lines):
       lines.sort()
