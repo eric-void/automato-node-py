19e296abdb12abb978b117049da5ec1a6a709909
Eric <eric@void.it>
2021-10-14 23:58:02 +0200

    Docs

--- docs/example-module.py
+++ docs/example-module.py
@@ -78,6 +78,7 @@ entry.actions = {} # INTERNO: usare system.entry_actions_supported(). Come sopra
 entry.on(event, listener, condition_eval = None, reference_entry = None, reference_tag = None)
 # def listener(entry, eventname, eventdata, caller, published_message): ... eventdata = { 'params': {...}, 'changed_params': {...} }, caller = "message|import|events_passthrough", published_message = source message (null for import)
 #   For eventdata description @see system._entry_event_publish
+# NOTE: eventdata is a local copy, so you can change it freely. Other params must not be changed (the changes will be propagated to next listeners and code)
 entry.do(action, params = {}, init_exec = None)
 
 # E' possibile arricchire entry con qualsiasi elemento si vuole mantenere tra le chiamate
@@ -295,6 +296,7 @@ definition = {
     "entry_id.event(condition)": { # condition è nella forma "js:params['value'] == 1 && params['value'] == 1" ...
       "handler": on_event, # def on_event(entry, eventname, eventdata, caller, published_message): eventdata = {'params': {...}, 'changed_params': {...}}, caller = "message|import|events_passthrough", published_message = source message (null for import)
         # For eventdata description @see system._entry_event_publish
+        # NOTE: eventdata is a local copy, so you can change it freely. Other params must not be changed (the changes will be propagated to next listeners and code)
       "script": [...], # Parametry passati: entry, on_entry, eventname, eventdata, params, caller, published_message, caller = "message|import|events_passthrough", published_message = source message (null for import). "entry" passato allo script è quello che ha impostato questa definition, mentre "on_entry" è quello specificato nell'"on" (del quale è stato preso l'evento)
       "do": "entry@NODE.action(init)", # init è nella forma "js:params['value'] = 1; params['value'] = 1;"
       "do": ["entry@NODE.action(init)", ...],
