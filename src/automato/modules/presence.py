86fbf0339aeb6608736847d25ed7e6cc202d3110
Eric <eric@void.it>
2022-04-23 22:54:35 +0200

    Presence: fix in code (unix newline)

--- src/automato/modules/presence.py
+++ src/automato/modules/presence.py
@@ -208,9 +208,9 @@ def locations_distance(origin, destination):
   Parameters
   ----------
   origin : tuple of float
- (lat, long)
+  (lat, long)
   destination : tuple of float
- (lat, long)
+  (lat, long)
 
   Returns
   -------
@@ -226,11 +226,11 @@ def locations_distance(origin, destination):
   lat1, lon1 = origin
   lat2, lon2 = destination
   radius = 6371
-  
+
   dlat = math.radians(lat2 - lat1)
   dlon = math.radians(lon2 - lon1)
   a = (math.sin(dlat / 2) * math.sin(dlat / 2) +
- math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) *
- math.sin(dlon / 2) * math.sin(dlon / 2))
+  math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) *
+  math.sin(dlon / 2) * math.sin(dlon / 2))
   c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
   return radius * c * 1000
