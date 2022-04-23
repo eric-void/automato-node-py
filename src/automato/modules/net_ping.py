6cd6c2be83ac65487cc2b94f1495e1c03c9f085c
Eric <eric@void.it>
2022-04-23 22:54:35 +0200

    Net ping module: added

---
+++ src/automato/modules/net_ping.py
@@ -0,0 +1,24 @@
+# require python3
+# -*- coding: utf-8 -*-
+
+# @see https://github.com/ValentinBELYN/icmplib/
+# @see https://github.com/ValentinBELYN/icmplib/blob/main/docs/6-use-icmplib-without-privileges.md
+
+import logging
+import icmplib
+
+definition = {
+  "description": _("Provide ping functionality via icmplib module"),
+  "config": {
+    "ping_timeout": 1,
+    "ping_privileged": False, # @see https://github.com/ValentinBELYN/icmplib/blob/main/docs/6-use-icmplib-without-privileges.md
+  }
+}
+
+def ping(entry, ip):
+  try:
+    h = icmplib.ping(ip, count = 1, timeout = entry.config['ping_timeout'], privileged = entry.config['ping_privileged'])
+    logging.debug("#{id}> pinged {ip}, is_alive: {is_alive}".format(id = entry.id, ip = ip, is_alive = h.is_alive))
+    return h.is_alive
+  except:
+    logging.exception("#{id}> failed executing ping".format(id = entry.id))
